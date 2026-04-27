310cd9e8940d09be73f0799fba5c81f18dbb8777
arbaz3a <marbaz00471@gmail.com>
2026-04-27 21:11:14 +0500

    cheee

--- multer/app.js
+++ multer/app.js
@@ -15,7 +15,7 @@ app.use(express.static('public'))
 // multer config
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads/');
+        cb(null, './public/images/uploads/');
     },
     filename: function (req, file, cb) {
         cb(null, Date.now() + path.extname(file.originalname));
