15e5fdca2ed02fc21926bcf3a3fe0b0b2a665e22
arbaz3a <marbaz00471@gmail.com>
2026-04-27 20:37:47 +0500

    learning

---
+++ multer/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static('public'))
+
+// multer config
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({ storage: storage });
+
+// routes
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    res.send('File uploaded successfully!');
+});
+
+// server
+app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
