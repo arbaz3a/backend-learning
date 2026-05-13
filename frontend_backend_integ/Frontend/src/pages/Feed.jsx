1c1da5ca321c25cd04633b7a0cc7f0f79c8a5e8e
arbaz3a <marbaz00471@gmail.com>
2026-05-13 18:00:14 +0500

    changes

--- frontend_backend_integ/Frontend/src/pages/Feed.jsx
+++ frontend_backend_integ/Frontend/src/pages/Feed.jsx
@@ -17,6 +17,21 @@ const Feed = () => {
         
     },[])  // Using this method, it shows the initial data on the first fetch, but it may fail to reflect new data added to the database immediately.
     
+        // // using polling
+    // useEffect(() => {
+    //   const fetchPosts = () => {
+    //     axios.get("http://localhost:3000/posts").then((res) => {
+    //       setPosts(res.data.posts);
+    //     });
+    //   };
+
+    //   fetchPosts(); // initial call
+
+    //   const interval = setInterval(fetchPosts, 5000); // every 5 sec
+
+    //   return () => clearInterval(interval);   // Cleanup interval when component unmounts
+    // }, []);  // This approach fetches data initially and then polls every 5 seconds, but there may be a delay in reflecting newly added database records.
+
     return (
 
         <section className='feed-section' >
